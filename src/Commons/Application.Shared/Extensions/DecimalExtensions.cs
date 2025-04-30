6a7148f2c10e52f9800c948fa15a4c9baeeb6a74
Emerson Trindade (menso) <menso@menso.dev.br>
2025-04-30 10:57:11 -0300

    melhora listagem de matérias primas

--- src/Commons/Application.Shared/Extensions/DecimalExtensions.cs
+++ src/Commons/Application.Shared/Extensions/DecimalExtensions.cs
@@ -1,8 +1,36 @@
+using System.Globalization;
+
 // ReSharper disable once CheckNamespace
 namespace Andromeda;
 
 public static class DecimalExtensions
 {
-    public static string ToCurrency(this decimal value) => value.ToString("C", CultureHelper.Brazilian);
-    public static string ToCurrency(this decimal? value) => value?.ToCurrency() ?? string.Empty;
+    private const string DefaultValue = "-";
+    
+    public static string ToCurrencyOrDefault(this decimal? value, string defaultValue = DefaultValue)
+    {
+        return value.HasValue ? value.Value.ToCurrency() : defaultValue;
+    }
+    
+    public static string ToCurrency(this decimal value)
+    {
+        return $"R$ {value.ToBrFormat()}";
+    }
+
+    public static string? ToBrFormat(this decimal? value)
+    {
+        if (!value.HasValue) { return ""; }
+        return value.Value.ToBrFormat();
+    }
+
+    public static string ToBrFormat(this decimal value)
+    {
+        return value.ToString(format: "#,##0.00", new CultureInfo("PT-BR"));
+    }
+
+    public static string ToPercentage(this decimal? value)
+    {
+        if (!value.HasValue) { return ""; }
+        return $"{value.ToBrFormat()}%";
+    }
 }
--- src/Commons/Client/Components/Pages/GslPageSection.razor.cs
+++ src/Commons/Client/Components/Pages/GslPageSection.razor.cs
@@ -5,5 +5,6 @@ public partial class GslPageSection : GslComponentBase
 {
     [Parameter] public string? Label { get; set; }
     [Parameter] public GslGap Gap { get; set; } = GslGap.Medium;
+    [Parameter] public bool NoPaper { get; set; }
     [Parameter, EditorRequired] public required RenderFragment ChildContent { get; set; }
 }
